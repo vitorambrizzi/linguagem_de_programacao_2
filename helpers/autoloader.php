03238aa769371da964ac65a1c8fe33d2e0588fc0
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-08-30 16:41:21 -0300

    Expanded autoloader paths

--- helpers/autoloader.php
+++ helpers/autoloader.php
@@ -4,6 +4,9 @@ function myLoader($class_name) {
     if (file_exists(CONTROLLERS_FOLDER . $class_name . '.php')) {
         require CONTROLLERS_FOLDER . $class_name . '.php';
     }
+    if (file_exists(HELPERS_FOLDER . $class_name . '.php')) {
+        require HELPERS_FOLDER . $class_name . '.php';
+    }
 }
 
 // Registra o myLoader para automatizar o carregamento das classes
