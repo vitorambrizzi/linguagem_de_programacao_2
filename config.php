5c3bfd10b59ad30ac4c0a1d315c9addc22e1e8ce
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-08-30 17:34:51 -0300

    Created model folder path constant

--- config.php
+++ config.php
@@ -3,5 +3,6 @@
 define('BASE_PATH','/projetos-ambrizzi/lp2/api-php-ifsp-2022-2/');
 define('CONTROLLERS_FOLDER', 'controllers/');
 define('HELPERS_FOLDER', 'helpers/');
+define('MODELS_FOLDER', 'models/');
 
 ?>
