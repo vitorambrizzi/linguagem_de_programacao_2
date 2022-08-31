9a4218b876f025125ab752d45b5f308932d7f160
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-08-30 21:14:21 -0300

    Fixed constants paths

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('BASE_PATH','/projetos-ambrizzi/lp2/api-php-ifsp-2022-2/');
-define('CONTROLLERS_FOLDER', 'controllers/');
-define('HELPERS_FOLDER', 'helpers/');
-define('MODELS_FOLDER', 'models/');
+define('BASE_PATH', '/projetos-ambrizzi/lp2/api-php-ifsp-2022-2/');
+define('CONTROLLERS_FOLDER', './controllers/');
+define('HELPERS_FOLDER', './helpers/');
+define('MODELS_FOLDER', './models/');
 
 ?>
