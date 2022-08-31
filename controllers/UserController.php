3a10e24f4d72d3c998c1dead506785ce372f01f7
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-08-30 22:12:07 -0300

    Added sign up action

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,8 +1,30 @@
 <?php
 
 class UserController {
-    function signup() {
-        echo 'Add User';
+    function signUp() {
+        // Get the inserts
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $pass = sha1($_POST['pass']);
+        $avatar = $_POST['avatar'];
+
+        // Inserts validation
+
+        // Implement database operation
+        $user = new User(null, $name, $email, $pass, $avatar);
+        $id = $user->create();
+
+        // Give the Output
+        $result['success']['message'] = 'User created successfully!';
+
+        $result['user']['id'] = $id;
+        $result['user']['name'] = $name;
+        $result['user']['email'] = $email;
+        $result['user']['pass'] = $pass;
+        $result['user']['avatar'] = $avatar;
+
+        $output = new Output();
+        $output->response($result);
     }
 }
 
