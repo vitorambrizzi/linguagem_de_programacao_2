7836755adde12d5dc49ffb741d48416f60bf5726
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-08-30 21:29:10 -0300

    Added Models folder path to autoloader

--- helpers/autoloader.php
+++ helpers/autoloader.php
@@ -7,6 +7,9 @@ function myLoader($class_name) {
     if (file_exists(HELPERS_FOLDER . $class_name . '.php')) {
         require HELPERS_FOLDER . $class_name . '.php';
     }
+    if (file_exists(MODELS_FOLDER . $class_name . '.php')) {
+        require MODELS_FOLDER . $class_name . '.php';
+    }
 }
 
 // Registra o myLoader para automatizar o carregamento das classes
