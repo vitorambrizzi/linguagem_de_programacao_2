feccf1eaee535b013954ef239d16570ca5243fdb
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-08-30 21:45:01 -0300

    Added Database class

--- helpers/Database.php
+++ helpers/Database.php
@@ -3,12 +3,25 @@
 class Database {
     function connect () {
         $servername = "localhost";
+        $dbname = "social_music";
         $username = "root";
         $password = "";
-        $dbname = "social_music";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            $this->dbError($e);
+        }
     }
 
-
+    function dbError ($e) {
+        $result['error']['message'] = 'Server error, please try again!';
+        $result['error']['database'] = $e;
+        $output = new Output();
+        $output->response($result, 500);
+    }
 }
 
 ?>
