c27c645633091305e0c5e2c7e91dfe9c8b739bf3
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-09-06 14:46:46 -0300

    Fixed base path constant

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_PATH', '/vitor_ambrizzi/linguagem_de_programacao_2-LP2I3/');
+define('BASE_PATH', '/vitor_ambrizzi/2022-2/linguagem_de_programacao_2-LP2I3/');
 define('CONTROLLERS_FOLDER', './controllers/');
 define('HELPERS_FOLDER', './helpers/');
 define('MODELS_FOLDER', './models/');
