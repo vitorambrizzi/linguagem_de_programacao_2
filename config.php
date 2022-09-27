ef2895cb92ba2842eb0e49684266005401e93d58
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-09-27 10:57:07 -0300

    Added database constants to config

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php
-// Paths and folders
+// PATHS AND FOLDERS
 define('BASE_PATH', '/vitor_ambrizzi/2022-2/linguagem_de_programacao_2-LP2I3/');
-define('CONTROLLERS_FOLDER', './controllers/');
-define('HELPERS_FOLDER', './helpers/');
-define('MODELS_FOLDER', './models/');
+define('CONTROLLERS_FOLDER', 'controllers/');
+define('HELPERS_FOLDER', 'helpers/');
+define('MODELS_FOLDER', 'models/');
+
+// DATABASE
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'social_music');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
 // CORS
 define('ALLOWED_HOSTS', 'http://localhost:3000');
--- helpers/Database.php
+++ helpers/Database.php
@@ -1,13 +1,8 @@
 <?php
 class Database {
     static function connect() {
-        $servername = "localhost";
-        $dbname = "social_music";
-        $username = "root";
-        $password = "";
-
         try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         } catch (PDOException $e) {
